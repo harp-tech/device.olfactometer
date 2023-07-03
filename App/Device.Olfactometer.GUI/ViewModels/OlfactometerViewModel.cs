c3b9ed3f9d1c0ec5cf4233b6605705d851781b1b
Luís Teixeira <micboucinha@gmail.com>
2023-07-03 14:43:30 +0100

    add flow related properties and UI elements

--- App/Device.Olfactometer.GUI/ViewModels/OlfactometerViewModel.cs
+++ App/Device.Olfactometer.GUI/ViewModels/OlfactometerViewModel.cs
@@ -9,11 +9,10 @@ using System.Reactive;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Reflection;
-using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Themes.Fluent;
 using Bonsai.Harp;
-using Device.Olfactometer.GUI.Models;
+using Harp.Olfactometer;
 using MessageBox.Avalonia.Enums;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
@@ -43,16 +42,59 @@ namespace Device.Olfactometer.GUI.ViewModels
         [Reactive] public HarpVersion HardwareVersion { get; set; }
         [Reactive] public HarpVersion FirmwareVersion { get; set; }
         
+        [Reactive] public EnableFlag EnableFlow { get; set; }
+        [ObservableAsProperty] public bool IsRunningFlow { get; }
+        
+        // device fields
+        [Reactive] public float Channel0FlowTarget { get; set; }
+        [Reactive] public float Channel0FlowReal { get; set; }
+        [Reactive] public float Channel1FlowTarget { get; set; }
+        [Reactive] public float Channel1FlowReal { get; set; }
+        [Reactive] public float Channel2FlowTarget { get; set; }
+        [Reactive] public float Channel2FlowReal { get; set; }
+        [Reactive] public float Channel3FlowTarget { get; set; }
+        [Reactive] public float Channel3FlowReal { get; set; }
+        [Reactive] public Channel3RangeConfig Channel3Range { get; set; }
+        [Reactive] public List<Channel3RangeConfig> Channel3RangeOptions { get; set; }
+        [Reactive] public float Channel4FlowTarget { get; set; }
+        [Reactive] public float Channel4FlowReal { get; set; }
+        
+        [Reactive] public ushort PulseValve0 { get; set; }
+        [Reactive] public ushort PulseValve1 { get; set; }
+        [Reactive] public ushort PulseValve2 { get; set; }
+        [Reactive] public ushort PulseValve3 { get; set; }
+        [Reactive] public ushort PulseEndValve0 { get; set; }
+        [Reactive] public ushort PulseEndValve1 { get; set; }
+
+        [Reactive] public bool Valve0State { get; set; }
+        [Reactive] public bool Valve1State { get; set; }
+        [Reactive] public bool Valve2State { get; set; }
+        [Reactive] public bool Valve3State { get; set; }
+        [Reactive] public bool EndValve0State { get; set; }
+        [Reactive] public bool EndValve1State { get; set; }
+        
+        [Reactive] public bool Valve0Toggle { get; set; }
+        [Reactive] public bool Valve1Toggle { get; set; }
+        [Reactive] public bool Valve2Toggle { get; set; }
+        [Reactive] public bool Valve3Toggle { get; set; }
+        [Reactive] public bool EndValve0Toggle { get; set; }
+        [Reactive] public bool EndValve1Toggle { get; set; }
+        
+        
+
+        
+        
         [Reactive] public bool ShowDarkTheme { get; set; }
         public ReactiveCommand<Unit, Unit> ChangeThemeCommand { get; }
         public ReactiveCommand<Unit, Unit> LoadDeviceInformation { get; }
         public ReactiveCommand<Unit, Unit> ConnectAndGetBaseInfoCommand { get; }
         
+        public ReactiveCommand<Unit, Unit> ToggleFlowCommand { get; }
+        
         private Harp.Olfactometer.AsyncDevice? _olfactometer;
         private readonly IObserver<HarpMessage> _observer;
         private IDisposable _observable;
         private readonly Subject<HarpMessage> _msgsSubject;
-        private DeviceConfiguration configuration;
 
         public OlfactometerViewModel()
         {
@@ -66,6 +108,7 @@ namespace Device.Olfactometer.GUI.ViewModels
 
             HarpMessages = new ObservableCollection<string>();
             ChangeThemeCommand = ReactiveCommand.Create(ChangeTheme);
+            Channel3RangeOptions = Enum.GetValues<Channel3RangeConfig>().ToList();
 
             LoadDeviceInformation = ReactiveCommand.CreateFromObservable(LoadUsbInformation);
             LoadDeviceInformation.IsExecuting.ToPropertyEx(this, x => x.IsLoadingPorts);
@@ -83,10 +126,28 @@ namespace Device.Olfactometer.GUI.ViewModels
                 //Log.Error(ex, "Error connecting to device with error: {Exception}", ex));
                 Console.WriteLine($"Error connecting to device with error: {ex}"));
             
+            var canChangeConfig = this.WhenAnyValue(x => x.Connected).Select(connected => connected);
+            ToggleFlowCommand = ReactiveCommand.CreateFromObservable(ToggleFlow, canChangeConfig);
+            ToggleFlowCommand.IsExecuting.ToPropertyEx(this, x => x.IsRunningFlow);
+            ToggleFlowCommand.ThrownExceptions.Subscribe(ex =>
+                //Log.Error(ex, "Error starting protocol with error: {Exception}", ex));
+                Console.WriteLine($"Error starting protocol with error: {ex}"));
+                
+            
             // force initial population of currently connected ports
             LoadUsbInformation();
         }
-        
+
+        private IObservable<Unit> ToggleFlow()
+        {
+            return Observable.StartAsync(async () =>
+            {
+                if (_olfactometer == null)
+                    throw new Exception("Olfactometer is not connected");
+                await _olfactometer.WriteEnableFlowAsync(EnableFlow);
+            });
+        }
+
         private void ChangeTheme()
         {
             Application.Current.Styles[0] = new FluentTheme(new Uri("avares://ControlCatalog/Styles"))
@@ -120,7 +181,6 @@ namespace Device.Olfactometer.GUI.ViewModels
                 if (string.IsNullOrEmpty(SelectedPort))
                     throw new Exception("invalid parameter");
 
-                configuration = new DeviceConfiguration();
                 if (_olfactometer != null)
                 {
                     _olfactometer.Dispose();
@@ -161,6 +221,45 @@ namespace Device.Olfactometer.GUI.ViewModels
                 FirmwareVersion = await _olfactometer.ReadFirmwareVersionAsync();
                 
                 Connected = true;
+                
+                // Read all the parameters and update corresponding properties
+                Channel0FlowTarget = await _olfactometer.ReadChannel0FlowTargetAsync();
+                Channel0FlowReal = await _olfactometer.ReadChannel0FlowRealAsync();
+                Channel1FlowTarget = await _olfactometer.ReadChannel1FlowTargetAsync();
+                Channel1FlowReal = await _olfactometer.ReadChannel1FlowRealAsync();
+                Channel2FlowTarget = await _olfactometer.ReadChannel2FlowTargetAsync();
+                Channel2FlowReal = await _olfactometer.ReadChannel2FlowRealAsync();
+                Channel3FlowTarget = await _olfactometer.ReadChannel3FlowTargetAsync();
+                Channel3FlowReal = await _olfactometer.ReadChannel3FlowRealAsync();
+                Channel4FlowTarget = await _olfactometer.ReadChannel4FlowTargetAsync();
+                Channel4FlowReal = await _olfactometer.ReadChannel4FlowRealAsync();
+                
+                PulseValve0 = await _olfactometer.ReadPulseValve0Async();
+                PulseValve1 = await _olfactometer.ReadPulseValve1Async();
+                PulseValve2 = await _olfactometer.ReadPulseValve2Async();
+                PulseValve3 = await _olfactometer.ReadPulseValve3Async();
+                PulseEndValve0 = await _olfactometer.ReadPulseEndvalve0Async();
+                PulseEndValve1 = await _olfactometer.ReadPulseEndvalve1Async();
+                
+                // get ValvesState
+                var valvesState = await _olfactometer.ReadValvesStateAsync();
+                Valve0State = valvesState.HasFlag(Valves.Valve0);
+                Valve1State = valvesState.HasFlag(Valves.Valve1);
+                Valve2State = valvesState.HasFlag(Valves.Valve2);
+                Valve3State = valvesState.HasFlag(Valves.Valve3);
+                EndValve0State = valvesState.HasFlag(Valves.EndValve0);
+                EndValve1State = valvesState.HasFlag(Valves.EndValve1);
+                
+                // get ValvesToggle
+                var valvesToggle = await _olfactometer.ReadValvesToggleAsync();
+                Valve0Toggle = valvesToggle.HasFlag(Valves.Valve0);
+                Valve1Toggle = valvesToggle.HasFlag(Valves.Valve1);
+                Valve2Toggle = valvesToggle.HasFlag(Valves.Valve2);
+                Valve3Toggle = valvesToggle.HasFlag(Valves.Valve3);
+                EndValve0Toggle = valvesToggle.HasFlag(Valves.EndValve0);
+                EndValve1Toggle = valvesToggle.HasFlag(Valves.EndValve1);
+
+                EnableFlow = await _olfactometer.ReadEnableFlowAsync();
 
                 //observable.Dispose();
 
