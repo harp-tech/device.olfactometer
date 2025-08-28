bca382283d38e96383070b675af9e9b38ebfc579
Luís Teixeira <micboucinha@gmail.com>
2025-08-28 08:27:01 +0100

    Add VisualStatus control and corresponding converter

---
+++ App/Harp.Olfactometer.Design/Controls/VisualStatus.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Harp.Olfactometer.Design.Controls
+{
+    public partial class VisualStatus : ContentControl
+    {
+        public static readonly StyledProperty<bool?> StatusProperty =
+            AvaloniaProperty.Register<VisualStatus, bool?>(nameof(Status), null);
+
+        public bool? Status
+        {
+            get => GetValue(StatusProperty);
+            set => SetValue(StatusProperty, value);
+        }
+
+        public VisualStatus()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
